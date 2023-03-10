cbfad5edfce8b353b3e1743e652aa9b59f7aed02
shem4soul@gmail.com <shem4soul@gmail.com>
2023-03-10 10:00:00 +0100

    Initial commit: GlowBeauty React MVC setup

---
+++ src/models/productModel.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Glow Nail Polish",
+    price: 2500,
+    category: "Nails",
+    image: "/images/nail-polish.jpg",
+  },
+  {
+    id: 2,
+    name: "Luxury Lip Gloss",
+    price: 3500,
+    category: "Cosmetics",
+    image: "/images/lip-gloss.jpg",
+  },
+  {
+    id: 3,
+    name: "Skin Radiance Serum",
+    price: 5500,
+    category: "Skincare",
+    image: "/images/serum.jpg",
+  },
+];
